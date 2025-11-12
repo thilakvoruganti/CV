19b4a8b5f64870b5ce76c443daf0cdfdc85464db
Thilak Voruganti <thilakvoruganti@Thilaks-MacBook-Pro.local>
2025-11-12 18:29:26 -0500

    Allow GitHub Pages via CORS regex

--- backend/app.py
+++ backend/app.py
@@ -14,8 +14,8 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
-        "https://thilakvoruganti.github.io",
     ],
+    allow_origin_regex=r"https://thilakvoruganti\.github\.io.*",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
