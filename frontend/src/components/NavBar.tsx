74188621dce22067b3cf2943869a946d6a16818e
thilakvoruganti <thilak.voruganti@gmail.com>
2026-02-21 11:10:05 -0500

    WIP save

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,21 +1,47 @@
+import { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import "./NavBar.css";
 
 export function NavBar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const closeMenu = () => setIsOpen(false);
+
   return (
     <header className="nav-root">
       <div className="nav-brand">
-        <Link to="/">World of Computer Vision</Link>
+        <Link to="/" onClick={closeMenu}>
+          World of Computer Vision
+        </Link>
       </div>
-      <nav className="nav-links">
-        <NavLink to="/" end>
+      <button
+        className="nav-toggle"
+        type="button"
+        aria-label="Toggle navigation"
+        aria-expanded={isOpen}
+        onClick={() => setIsOpen((prev) => !prev)}
+      >
+        Menu
+      </button>
+      <nav className={`nav-links${isOpen ? " open" : ""}`}>
+        <NavLink to="/" end onClick={closeMenu}>
           Home
         </NavLink>
-        <NavLink to="/measure">Measure Lab</NavLink>
-        <NavLink to="/stitch">Image Stitching</NavLink>
-        <NavLink to="/sift">SIFT Compare</NavLink>
-        <NavLink to="/module2">Module 2</NavLink>
-        <NavLink to="/module3">Module 3</NavLink>
+        <NavLink to="/measure" onClick={closeMenu}>
+          Measure Lab
+        </NavLink>
+        <NavLink to="/stitch" onClick={closeMenu}>
+          Image Stitching
+        </NavLink>
+        <NavLink to="/sift" onClick={closeMenu}>
+          SIFT Compare
+        </NavLink>
+        <NavLink to="/module2" onClick={closeMenu}>
+          Module 2
+        </NavLink>
+        <NavLink to="/module3" onClick={closeMenu}>
+          Module 3
+        </NavLink>
       </nav>
     </header>
   );
